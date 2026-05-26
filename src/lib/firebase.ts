4b49b080f1ab5d456682f4857f0ca2b03f0bf7d6
Omar Rawas <omar.rawas17@gmail.com>
2026-05-26 16:18:57 +0300

    13

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDJMfencP299e89zPg31MaVKdQS489cgzM",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "taskaty-f87b9.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "taskaty-f87b9",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "taskaty-f87b9.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "830856372835",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:830856372835:web:07bbe602b90e4f226b8e56",
 };
 
 const app = initializeApp(firebaseConfig);
